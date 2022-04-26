aa304655fde764ceee081bb4e839f7a254756719
Luke <llogan382@gmail.com>
2022-04-26 11:35:23 -0400

    image imports

--- components/GridGallery.jsx
+++ components/GridGallery.jsx
@@ -1,26 +1,26 @@
 import Image from "next/image";
-import one from "../../public/1.jpg";
-import two from "../../public/2.jpg";
-import three from "../../public/3.jpg";
-import four from "../../public/4.jpg";
-import five from "../../public/5.jpg";
-import six from "../../public/6.jpg";
-import seven from "../../public/7.jpg";
-import eight from "../../public/8.jpg";
-import nine from "../../public/9.jpg";
-import ten from "../../public/10.jpg";
-import eleven from "../../public/11.jpg";
-import twelve from "../../public/12.jpg";
-import thirteen from "../../public/13.jpg";
-import fourteen from "../../public/14.jpg";
-import fifteen from "../../public/15.jpg";
-import sixteen from "../../public/16.jpg";
-import seventeen from "../../public/17.jpg";
-// import 18 from '../../public/18.jpg'
-// import 19 from '../../public/19.jpg'
-// import 20 from '../../public/20.jpg'
-// import 21 from '../../public/21.jpg'
-// import 22 from '../../public/22.jpg'
+import one from "../public/1.jpg";
+import two from "../public/2.jpg";
+import three from "../public/3.jpg";
+import four from "../public/4.jpg";
+import five from "../public/5.jpg";
+import six from "../public/6.jpg";
+import seven from "../public/7.jpg";
+import eight from "../public/8.jpg";
+import nine from "../public/9.jpg";
+import ten from "../public/10.jpg";
+import eleven from "../public/11.jpg";
+import twelve from "../public/12.jpg";
+import thirteen from "../public/13.jpg";
+import fourteen from "../public/14.jpg";
+import fifteen from "../public/15.jpg";
+import sixteen from "../public/16.jpg";
+import seventeen from "../public/17.jpg";
+// import 18 from '../public/18.jpg'
+// import 19 from '../public/19.jpg'
+// import 20 from '../public/20.jpg'
+// import 21 from '../public/21.jpg'
+// import 22 from '../public/22.jpg'
 
 export default function GridGallery() {
   return (
