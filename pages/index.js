8cecf39a7ce57ea7ea78c4077edfb3cae83f52cc
Luke <llogan382@gmail.com>
2022-04-26 12:02:15 -0400

    chore: adds WIP to preview branch

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
+          aliquip ex ea commodo.
         </div>
       </section>
       <section>
