0e07a196a9aac492635d98973c7dba1d89ab70dd
Luke <llogan382@gmail.com>
2022-04-27 12:55:44 -0400

    adds video

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
 import PTCHeader from "../components/PTCHeader";
 import GridGallery from "../components/GridGallery";
-
+import Image from "next/image";
+import PTCVideo from "../components/PTCVideo";
+import three from "../public/three.jpg";
+import ReactPlayer from "react-player";
+import React from "react";
 export default function Home() {
   return (
     <>
       <PTCHeader />
-      <section>
+
+      <PTCVideo />
+      <section className="header_wrapper">
         <p className="headerContent">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
@@ -13,17 +19,69 @@ export default function Home() {
           aliquip ex ea commodo.
         </p>
       </section>
-      <section>
-        <div className="headerContent"></div>
+      <section className="meet_pat_section">
+        <div className="meet_pat_wrapper">
+          <div className="meet_pat_content">
+            <h3>Meet Pat</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehend
+            </p>
+          </div>
+          <div className="meet_pat_img">
+            <Image
+              // layout="fill"
+              width={1500}
+              height={1200}
+              className="navLogo"
+              src={three}
+              alt="Pat Tillman Logo"
+            />
+          </div>
+        </div>
       </section>
       <GridGallery />
       <style jsx>{`
-        section {
-          padding: 2rem 0;
-
+        .header_wrapper {
           display: flex;
           max-width: 1200px;
           margin: auto;
+        }
+
+        .meet_pat_section {
+          width: 100%;
+          color: white;
+          background-color: var(--dark-red);
+        }
+
+        .meet_pat_wrapper {
+          max-width: 1200px;
+          margin: auto;
+          display: flex;
+          flex-direction: row;
+          justify-content: space-evenly;
+        }
+        .meet_pat_content {
+          padding: 2rem 0;
+          display: flex;
+          flex-direction: column;
+        }
+        .meet_pat_img {
+        }
+
+        .player-wrapper {
+          position: relative;
+          border: 5px solid red;
+          padding-top: 56.25%; /* Player ratio: 100 / (1280 / 720) */
+        }
+
+        .react-player {
+          position: absolute;
+          top: 0;
+          left: 0;
         }
       `}</style>
     </>
